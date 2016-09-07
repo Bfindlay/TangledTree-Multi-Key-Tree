f6fa4f82e2b8c12f6f77d531e23943aa712a0762
bfindlay <bfin5330@uni.sydney.edu.au>
2016-09-07 19:13:53 +1000

    added interface methods for a node

--- src/tangleTree/Node.java
+++ src/tangleTree/Node.java
@@ -11,4 +11,21 @@ public interface Node<J, K, V> {
 	public Node<J, K, V> pullRight();
 
 	public Node<J, K, V> pullLeft();
+
+	public Node<J, K, V> setParent(Node<K, J, V> node);
+
+	public Node<J, K, V> addParent(Node<K, J, V> node);
+
+	public Node<J, K, V> getParent();
+
+	public Node<J, K, V> pullParent();
+
+	public Node<J, K, V> addLeft(Node<K, J, V> node);
+
+	public Node<J, K, V> addRight(Node<K, J, V> node);
+
+	public Node<J, K, V> setLeft(Node<K, J, V> node);
+
+	public Node<J, K, V> setRight(Node<K, J, V> node);
+
 }
