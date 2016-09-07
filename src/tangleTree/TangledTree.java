dc58b171400723fda5a1bea7b155b21edd843af1
bfindlay <bfin5330@uni.sydney.edu.au>
2016-09-07 19:14:37 +1000

    implementing interface

--- src/tangleTree/TangledTree.java
+++ src/tangleTree/TangledTree.java
@@ -1,21 +1,57 @@
 package tangleTree;
 
-public interface TangledTree<K, J, V> {
+public class TangledTree<J, K, V> implements TangleTree<K, J, V> {
 
-	public Node<K, J, V> insert(Node<K, J, V> node);
+	public TangledTree() {
 
-	public boolean isEmpty();
+	}
 
-	public int size();
+	@Override
+	public Node insert(Node node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-	public void clear();
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-	public boolean contains(Node<K, J, V> node);
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-	public Node<K, J, V> get(K key);
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
 
-	public Node<K, J, V> pull(J key);
+	}
 
-	public Node<K, J, V> remove(Node<K, J, V> node);
+	@Override
+	public boolean contains(Node node) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Node get(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node pull(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node remove(Node node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
