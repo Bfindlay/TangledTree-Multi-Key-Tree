b632265295bcc8330ec02c01176d57e17cad42f7
bfindlay <bfin5330@uni.sydney.edu.au>
2016-09-08 11:44:42 +1000

    passing basic tests

--- test/test/TangledTreeTest.java
+++ test/test/TangledTreeTest.java
@@ -13,11 +13,33 @@ public class TangledTreeTest {
 		TangledTree<Integer, String, String> tree = new TangledTree<>();
 
 		assertEquals(0, tree.size());
-		tree.insert(10, "A", "HI");
+		tree.insert(10, "F", "HI");
 		assertEquals(1, tree.size());
 		assertEquals("HI", tree.get(10));
-		assertEquals("HI", tree.pull("A"));
+		assertEquals("HI", tree.pull("F"));
+		tree.insert(5, "C", "BYE");
+		assertEquals(2, tree.size());
+		assertEquals("BYE", tree.get(5));
+		assertEquals("BYE", tree.pull("C"));
 
+	}
+
+	@Test
+	public void testIsEmpty() {
+		TangledTree<Integer, String, String> tree = new TangledTree<>();
+
+		assertTrue(tree.isEmpty());
+		tree.insert(10, "A", "HI");
+		assertFalse(tree.isEmpty());
+	}
+
+	@Test
+	public void testSize() {
+		TangledTree<Integer, String, String> tree = new TangledTree<>();
+
+		assertEquals(0, tree.size());
+		tree.insert(10, "A", "HI");
+		assertEquals(1, tree.size());
 	}
 
 }
